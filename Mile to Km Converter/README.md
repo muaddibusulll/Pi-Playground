807e7d4bd8ad0d9af70bd435232006f649c3656a
sifisKoen <sifis.k.i@gmail.com>
2023-04-13 22:19:54 +0200

    tkinter first try Converter

---
+++ Mile to Km Converter/README.md
@@ -0,0 +1,32 @@
+# Converter
+
+This is a simple converter just to make me familiar with the **tkinter** python library. And how to manipulate the widgets.
+
+## Tools 🪛
+
+As I mentioned I used a build in Python library **tkinter** for my graphics. This library is specific for GUIs in python.
+
+## How to run it ⚙️
+
+It's very simple
+
+- First you need to to have **Python 3** installed. (You probably have it).
+- Then you need to _clone_ my repository.
+  - Use `git clone <the name of the repo>`
+
+* Now you should have the whole repository in your machine.
+
+  - Now go to the **/Mile to Km Converter** directory and open your terminal.
+    - From your terminal type, `python3 main.py`
+
+## Final thoughts
+
+**tkinter** is a very simple and nice library to use for simple GUI programs in Python.
+
+## Useful Documentation:
+
+- [tkinter documentation][tkinter]
+- [pack documentation][pack]
+
+[tkinter]: https://docs.python.org/3/library/tkinter.html#the-packer
+[pack]: https://tcl.tk/man/tcl8.6/TkCmd/pack.htm
---
+++ Mile to Km Converter/main.py
@@ -0,0 +1,50 @@
+import tkinter
+
+
+def convert_miles_to_kilometers():
+    miles = float(miles_entry.get())
+    convert = str(round(miles / 0.62137))
+    kilometers_output.config(text=convert)
+
+
+LABELS_FONT = ("Arial", 15)
+
+# Window configuration
+window = tkinter.Tk()
+window.title("My first program")
+window.minsize(width=300, height=150)
+window.config(padx=30, pady=30)
+
+# Label
+my_label = tkinter.Label(text="is equal to",
+                         font=LABELS_FONT)
+my_label.grid(column=0, row=1)
+
+# Column 1
+
+# Entry
+miles_entry = tkinter.Entry(width=10)
+miles_entry.insert(tkinter.END, string="0")
+miles_entry.grid(column=1, row=0)
+
+# Label for the convert
+kilometers_output = tkinter.Label(text="0", font=LABELS_FONT)
+kilometers_output.grid(column=1, row=1)
+
+# Button
+convert_button = tkinter.Button(
+    text="Convert", command=convert_miles_to_kilometers)
+convert_button.grid(column=1, row=2)
+
+# Column 2
+
+# Label for miles
+miles = tkinter.Label(text="Miles", font=LABELS_FONT)
+miles.grid(column=2, row=0)
+
+# Label for kilometers
+kilometers = tkinter.Label(text="Km", font=LABELS_FONT)
+kilometers.grid(column=2, row=1)
+
+
+window.mainloop()

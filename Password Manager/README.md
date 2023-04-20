99101d05b9123520c1a493f6cedf9232fc1ec168
sifisKoen <sifis.k.i@gmail.com>
2023-04-20 16:58:01 +0200

    Completed Password Manager plus README.md

---
+++ Password Manager/README.md
@@ -0,0 +1,45 @@
+# Password Creator and Manager 🔐
+
+This is UI Python program witch creates a random password for a web site and save the name of the site as well as the password and the email address of the user into a txt file **locally** to user's computer.
+
+The name of the file is **data.txt** witch very easily can be change from the code.
+
+## Tools 🪛
+
+I used the build in Python library **tkinter** for the graphics (UI).
+
+From **tkinter** I used additionally the **messagebox** module for displaying messages to the user's screen.
+
+Also I used from the **PiPy** library the **pyperclip** witch helps when the user push the button _Generate Password_ and the new password generate to copy this new password to user's clipboard.
+
+## How to run it ⚙️
+
+It's very simple
+
+- First you need to to have **Python 3** installed. (You probably have it).
+- Then you need to _clone_ my repository.
+  - Use `git clone <the name of the repo>`
+
+* Now you should have the whole repository in your machine.
+
+  - Now go to the **/Password Manager** directory and open your terminal.
+    - From your terminal type, `python3 main.py`
+
+* Very important IF you run Linux machine first you need to install: `sudo apt-get install xsel` this is copy paste mechanism witch let the **pyperclip** run correctly. This is only for Linux users for Mac and Windows users works without any installations.
+
+## The structure of the project 📚
+
+    ├── data.txt
+    ├── logo.png
+    ├── main.py
+    └── README.md
+
+## Class Explanation 📖
+
+In this project we have only one class.
+
+- main.py
+  - I our main class where all the program runs. In this class we have tree concrete sections.
+    - **_PASSWORD GENERATOR_** witch is the section where the password **generates** when the user clicks on the button _Generate Password_. This section is divided into three different functions, `password_characters_lists()`, `manipulate_password_characters()`, and `password_creation()`
+    - **_SAVE PASSWORD_** in this section the user's input saved into a txt file if the file is already created but if the file is not there it also creates a file automatically. This section is divided into five different functions, `is_a_field_blank()`, `empty_fields()`, `confirmation_message_box()`, `file()`, `save_data_to_file()`
+    - **_UI SETUP_** in this last but very important section lives our UI and all the commands of it. In this section all previous functions get called so to make use of them.
--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,9 @@
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
+import string
+import random
+import pyperclip
 
 # Constants
 WEBSITE_LABEL_TEXT = "Website:"
@@ -12,21 +16,98 @@ FONT = (FONT_NAME, 12)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def password_characters_lists():
+
+    password_letters_list = [(random.choice(string.ascii_letters))
+                             for letter in range(random.randint(2, 4))]
+    password_digits_list = [(random.choice(string.digits))
+                            for digit in range(random.randint(2, 4))]
+    password_symbols_list = [(random.choice(string.punctuation))
+                             for punctuation in range(random.randint(2, 4))]
+
+    return password_letters_list, password_digits_list, password_symbols_list
+
+
+def manipulate_password_characters():
+
+    password_letters_list = password_characters_lists()[0]
+    password_digits_list = password_characters_lists()[1]
+    password_symbols_list = password_characters_lists()[2]
+
+    password_list = password_letters_list + \
+        password_digits_list + password_symbols_list
+
+    random.shuffle(password_list)
+
+    return password_list
+
+
+def password_creation():
+
+    password_shuffled_list = manipulate_password_characters()
+
+    new_password = ''.join(password_shuffled_list)
+
+    password_import_text.insert(tkinter.END, string=new_password)
+    pyperclip.copy(new_password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# TODO: Create a function that will create a new file
-# If it doesn't exist or will open it.
-# Pass this file name to an other function
-# LINK: https://www.w3schools.com/python/python_file_write.asp
 
-# Or combine them into one function... 🤔
+def is_a_field_blank(website, email, password):
 
-# TODO: Create a function that will take all the fields
-# from the program and will save them into the provided file.
-# Into this function the fields will be cleared up after
-# each time the function is called.
-# def save_password():
+    if (website == ""):
+        messagebox.showwarning(
+            title="Warning", message="Please enter a website")
+        return True
+    elif (password == ""):
+        messagebox.showwarning(
+            title="Warning", message="Please enter a password")
+        return True
+    elif (email == ""):
+        messagebox.showwarning(
+            title="Warning", message="Please enter an email address")
+        return True
+    else:
+        return False
+
+
+def empty_fields():
+    website_import_text.delete(0, "end")
+    password_import_text.delete(0, "end")
+
+
+def confirmation_message_box(website, email, password):
+
+    return messagebox.askokcancel(
+        title="Confirm", message=f"These details are correct?\nWebsite: {website}\nEmail: {email}\nPassword: {password}\nIs it ok to save?")
+
+
+def file():
+    my_passwords_file = open("data.txt", "a")
+    return my_passwords_file
+
 # LINK: https://tkdocs.com/tutorial/widgets.html#entry
+
+
+def save_data_to_file():
+
+    website = website_import_text.get()
+    email_username = email_username_import_text.get()
+    password = password_import_text.get()
+
+    if (is_a_field_blank(website=website, email=email_username, password=password) == True):
+        return
+
+    if (confirmation_message_box(website=website, email=email_username, password=password) == True):
+        file().write(
+            f"{website.capitalize()} | {email_username} | {password}\n")
+        file().close()
+        print(f"{website} | {email_username} | {password}")
+
+        empty_fields()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,12 +151,13 @@ password_import_text = tkinter.Entry(width=21)
 password_import_text.grid(row=3, column=1, sticky="EW")
 
 generate_password_button = tkinter.Button(
-    text=GENERATE_PASSWORD_BUTTON_TEXT)
+    text=GENERATE_PASSWORD_BUTTON_TEXT, command=password_creation)
 generate_password_button.grid(row=3, column=2, sticky="EW")
 
 # -------------------- Add Button import area -------------------- #
 
-add_button = tkinter.Button(text=ADD_BUTTON_TEXT, width=36)
+add_button = tkinter.Button(
+    text=ADD_BUTTON_TEXT, width=36, command=save_data_to_file)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 width = window.mainloop()

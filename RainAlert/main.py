4c98ea7da60535b7c4c1353bfd0905a92ceac189
sifisKoen <sifis.k.i@gmail.com>
2023-06-20 00:18:10 +0200

    Add if will rain functionality

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -22,5 +22,38 @@ forecast_API_response = requests.get(f"https://api.openweathermap.org/data/2.8/o
 
 forecast_API_response.raise_for_status()
 
-for weather in range(len(forecast_API_response.json()["hourly"])):
-    print(forecast_API_response.json()["hourly"][weather]["weather"])
+weather_hourly = forecast_API_response.json()["hourly"]
+
+def forecast_output(weather_id, will_rain):
+    if weather_id >= 200 and weather_id < 300:
+        print("It rain heavily\nYou should need an umbrella")
+        will_rain = True
+        return will_rain
+    elif weather_id >= 300 and weather_id < 500:
+        print("It will drizzle\nYou should need an umbrella")
+        will_rain = True
+        return will_rain
+    elif weather_id >= 500 and weather_id < 600:
+        print("It will rain\nYou should need an umbrella")
+        will_rain = True
+        return will_rain
+    elif weather_id >= 600 and weather_id < 700:
+        return("It will snow")
+    elif weather_id >= 700 and weather_id < 800:
+        return("It will have fog")
+    elif weather_id == 800:
+        return("It will be clear sky")
+    else:
+        return("It will have clouds")
+
+will_rain = False
+
+for weather in range(len(weather_hourly[:12])):
+    weather_id = weather_hourly[weather]["weather"][0]["id"]
+    final_output = forecast_output(weather_id, will_rain=will_rain)
+    if final_output == True:        
+        print(final_output)
+        break
+    else:
+        print(final_output)
+        break
